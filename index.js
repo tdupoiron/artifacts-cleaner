7ad4c67c09a0445a21ff18abae874a0f9f045e19
Thomas Dupoiron <tdupoiron@github.com>
2022-11-25 09:55:57 +0100

    simple javascript file to extract all artifacts from an org

---
+++ index.js
@@ -0,0 +1,70 @@
+// Require libraries
+const { Octokit } = require("@octokit/rest");
+
+// Init constants
+const org = "tdupoiron-org";
+const octokit = new Octokit(
+    {
+        auth: process.env.GITHUB_TOKEN,
+        baseUrl: "https://api.github.com"
+    }
+);
+
+// List all repositories
+async function getRepos(org) {
+
+    return octokit.rest.repos.listForOrg({
+        org: org,
+        type: "private"
+    }).then(({ data, headers, status }) => {
+        return data;
+    });
+};
+
+// List all artifacts
+async function getArtifacts(owner, repo) {
+
+    return octokit.rest.actions.listArtifactsForRepo({
+        owner: owner,
+        repo: repo,
+        per_page: 100
+    }).then(({ data, headers, status }) => {
+        return data;
+    }
+    );
+};
+
+async function main() {
+
+    var artifactsOut = [];
+
+    // Get all repositories
+    const repos = await getRepos(org);
+
+    // Get all artifacts
+    for (const i in repos)
+    {
+        const currentRepo = repos[i];
+        const artifacts = await getArtifacts(org, currentRepo.name);
+
+        // Build output format for each artifact
+        for (const j in artifacts.artifacts)
+        {
+            const currentArtifact = artifacts.artifacts[j];
+
+            const output = {
+                "repoId": currentRepo.id,
+                "repoName": currentRepo.name,
+                "artifactId": currentArtifact.id,
+                "artifactName": currentArtifact.name,
+                "size": currentArtifact.size_in_bytes,
+                "created_at": currentArtifact.created_at,
+                "expires_at": currentArtifact.expires_at
+            };
+
+            artifactsOut.push(output);
+        }
+    }
+}
+
+main();
